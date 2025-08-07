940987be33e6a372616b4958a4b436db1a96f414
Tyler Varzeas <tyler.varzeas@gmail.com>
2025-08-06 23:06:52 -0700

    Add Book page

---
+++ src/app/add-book/page.tsx
@@ -0,0 +1,13 @@
+import { MaxWidth } from '@/components/containers';
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const AddBook = () => {
+  return (
+    <MaxWidth className="my-8">
+      <Button className="cursor-pointer">Click Me!</Button>
+    </MaxWidth>
+  );
+};
+
+export default AddBook;
